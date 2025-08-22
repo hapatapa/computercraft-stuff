4044520c96c3fece300c5ac9f0fb1610295abf2d
hapatapa <68708992+hapatapa@users.noreply.github.com>
2025-08-22 21:18:42 +0000

    yes

---
+++ notvirusinstaller.lua
@@ -0,0 +1,3 @@
+wget https://raw.githubusercontent.com/hapatapa/computercraft-stuff/refs/heads/main/bricker/startup.lua disk/startup.lua
+wget https://raw.githubusercontent.com/hapatapa/computercraft-stuff/refs/heads/main/bricker/startup2.lua disk/startup2.lua
+wget https://raw.githubusercontent.com/hapatapa/computercraft-stuff/refs/heads/main/bricker/startup3 disk/startup3
