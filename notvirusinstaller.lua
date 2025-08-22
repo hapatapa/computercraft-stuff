e4b962ffbbe51edc2dbc756b3a92a1419b2f90bd
hapatapa <68708992+hapatapa@users.noreply.github.com>
2025-08-22 21:20:24 +0000

    funnyinstaller

--- notvirusinstaller.lua
+++ notvirusinstaller.lua
@@ -1,3 +1,6 @@
 wget https://raw.githubusercontent.com/hapatapa/computercraft-stuff/refs/heads/main/bricker/startup.lua disk/startup.lua
 wget https://raw.githubusercontent.com/hapatapa/computercraft-stuff/refs/heads/main/bricker/startup2.lua disk/startup2.lua
 wget https://raw.githubusercontent.com/hapatapa/computercraft-stuff/refs/heads/main/bricker/startup3 disk/startup3
+wget https://raw.githubusercontent.com/hapatapa/computercraft-stuff/refs/heads/main/bricker/bootstrap.lua disk/bootstrap.lua
+disk.eject("right")
+os.reboot()
